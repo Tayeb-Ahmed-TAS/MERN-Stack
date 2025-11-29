5ae5b2e0d3f3c75ea9635a7295927171e525e7e0
Tayeb Ahmed <atayeb.english939@gmail.com>
2025-11-29 15:44:46 +0600

    Class and Inheritance

---
+++ Js/OOPs/script.js
@@ -0,0 +1,47 @@
+class Person {
+  constructor(name, age, department) {
+    this.name = name;
+    this.age = age;
+    this.department = department;
+  }
+
+  greet() {
+    console.log(`Hi, I'm ${this.name}`);
+  }
+}
+
+class Student extends Person {
+  constructor(name, age, id, department) {
+    super(name, age, department);
+    this.id = id;
+  }
+}
+
+class Teacher extends Person {
+  constructor(name, age, department, subject) {
+    super(name, age, department);
+    this.subject = subject;
+  }
+}
+
+const p1 = new Student("John", 20, "S123", "Computer Science");
+const p2 = new Teacher("Ms. Smith", 35, "CSE", "Algorithms");
+
+p1.greet();
+console.log(p1);
+
+p2.greet();
+console.log(p2);
+
+const p3 = new Teacher("Mr. Brown", 45, "Math", ["Calculus", "Algebra"]);
+p3.greet();
+console.log(p3);
+
+const p4_subjects = ["Quantum Mechanics", "Thermodynamics", "Optics"];
+const p4 = new Teacher("Dr. Green", 50, "Physics", [
+  "Quantum Mechanics",
+  "Thermodynamics",
+  "Optics",
+]);
+p4.greet();
+console.log(p4);
