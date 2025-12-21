44de9701e9ee849cc2979cd322f61d34228f18ca
Tayeb Ahmed <atayeb.english939@gmail.com>
2025-12-21 12:57:31 +0600

    Routes for listing and review

---
+++ Projects/AirBnb/routes/listing.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const router = express.Router();
+const Listing = require("../models/listing.js");
+const wrapAsync = require("../utils/wrapAsync.js");
+const ExpressError = require("../utils/ExpressError.js");
+const { listingSchema } = require("../schema.js");
+
+// Middleware to validate listing data using Joi schema
+const validateListing = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(", ");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
+
+// Index Route - Show all listings
+router.get(
+  "/",
+  wrapAsync(async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
+  })
+);
+
+// New Route - Form to create a new listing (Keep it above Show Route otherwise `new` will be treated as :id)
+
+router.get("/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
+
+// Show Route - Show details of a specific listing
+router.get(
+  "/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    let listing = await Listing.findById(id).populate("reviews");
+    res.render("listings/show.ejs", { listing });
+  })
+);
+
+// Create Route - Create a new listing
+router.post(
+  "/",
+  validateListing,
+  wrapAsync(async (req, res) => {
+    let newListing = new Listing(req.body.listing); // because in form we have used listing[title], listing[description]...
+    await newListing.save();
+    res.redirect("/listings");
+  })
+);
+
+// Edit Route - Form to edit an existing listing
+router.get(
+  "/:id/edit",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    let listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+  })
+);
+
+// Update Route - Update an existing listing
+router.put(
+  "/:id",
+  validateListing,
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing }); // Destructuring to get all fields from req.body.listing
+    res.redirect(`/listings/${id}`); // Redirect to show page
+  })
+);
+
+// Delete Route - Delete a listing
+router.delete(
+  "/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+  })
+);
+
+module.exports = router;
---
+++ Projects/AirBnb/routes/review.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const router = express.Router({ mergeParams: true }); // mergeParams to access :id from parent route
+const Listing = require("../models/listing.js");
+const Review = require("../models/review.js");
+const wrapAsync = require("../utils/wrapAsync.js");
+const ExpressError = require("../utils/ExpressError.js");
+const {  reviewSchema } = require("../schema.js");
+
+// Middleware to validate review data using Joi schema
+const validateReview = (req, res, next) => {
+  let { error } = reviewSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(", ");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
+
+// Create Review Route
+
+router.post(
+  "/",
+  validateReview,
+  wrapAsync(async (req, res) => {
+    const { id } = req.params;
+    let listing = await Listing.findById(id);
+    let newReview = new Review(req.body.review); // Adding review to Review collection
+    listing.reviews.push(newReview); // Pushing review to listing's reviews array
+
+    // Save both listing and review
+    await newReview.save();
+    await listing.save();
+
+    res.redirect(`/listings/${id}`);
+  })
+);
+
+// Delete Review Route
+
+router.delete(
+  "/:reviewId",
+  wrapAsync(async (req, res) => {
+    let { id, reviewId } = req.params;
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } }); // Removing review reference from listing's reviews array
+    await Review.findByIdAndDelete(reviewId); // Deleting review from Review collection
+    res.redirect(`/listings/${id}`);
+  })
+);
+
+module.exports = router;
